fb3fad10dc9f0f25c48533e7f1b9703717b7a96c
Tootha <tutic.marko@gmail.com>
2023-02-24 12:12:58 +0100

    Replaced sigmoid with softmax

--- classifier/code/model.py
+++ classifier/code/model.py
@@ -1,6 +1,5 @@
 import torch.nn as nn
 
-# Basic block of ResNet18 network
 class BasicBlock(nn.Module):
     expansion = 1
 
@@ -10,7 +9,8 @@ class BasicBlock(nn.Module):
         self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
-
+        self.relu1 = nn.ReLU()
+        self.relu2 = nn.ReLU()
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
@@ -19,13 +19,13 @@ class BasicBlock(nn.Module):
             )
 
     def forward(self, x):
-        out = nn.ReLU()(self.bn1(self.conv1(x)))
+        out = self.relu1(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
         out += self.shortcut(x)
-        out = nn.ReLU()(out)
+        out = self.relu2(out)
         return out
 
-# ResNet-18 Module with a change where input contains only one channel instead 3 as described in paper
+
 class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10):
         super(ResNet, self).__init__()
@@ -38,7 +38,9 @@ class ResNet(nn.Module):
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         self.linear = nn.Linear(512*block.expansion, num_classes)
-        self.sigmoid = nn.Sigmoid()
+        self.relu = nn.ReLU()
+        self.adapool = nn.AdaptiveAvgPool2d((1, 1))
+        self.softmax = nn.Softmax(dim=1)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -49,15 +51,15 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out = nn.ReLU()(self.bn1(self.conv1(x)))
+        out = self.relu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        out = nn.AdaptiveAvgPool2d((1, 1))(out)
+        out = self.adapool(out)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
-        out = self.sigmoid(out)
+        out = self.softmax(out)
         return out
 
 
